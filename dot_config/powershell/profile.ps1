7e2b309f5190c8feccf9506a0d46643c6aeb4ccb
Julie Koubova <julie@koubova.net>
2023-11-01 17:12:36 -0700

    vswhere

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -1,8 +1,11 @@
 If ($IsWindows) {
-  If (Get-Command vs -ea SilentlyContinue) {
-    $VSPath = @(vs where preview -prop=InstallationPath)[-1]
-    Import-Module (Join-Path $VSPath Common7\Tools\Microsoft.VisualStudio.DevShell.dll)
-    $Null = Enter-VsDevShell -VsInstallPath $VSPath -StartInPath (Get-Location) 
+  $VSWhere = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+  If (Get-Command $VSWhere -ea SilentlyContinue) {
+    $VSPath = @(& $VSWhere -latest -prerelease -property InstallationPath 2>$null)[-1]
+    If ($VSPath) {
+      Import-Module (Join-Path $VSPath Common7\Tools\Microsoft.VisualStudio.DevShell.dll)
+      $Null = Enter-VsDevShell -VsInstallPath $VSPath -StartInPath (Get-Location) 
+    }
   }
 
   If (Get-Command python -ea SilentlyContinue) {
