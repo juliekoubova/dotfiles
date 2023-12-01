58ed8e87cefb35ed1efe0db9367ffede08dd4e2d
Julie Koubova <julie@koubova.net>
2023-12-01 20:54:58 +0100

    doskey

---
+++ .chezmoiscripts/run_once_cmd_autorun.cmd
@@ -0,0 +1,9 @@
+@echo off
+echo Setting .autorun.cmd as Command Processor Autorun
+reg add ^
+  "HKCU\SOFTWARE\Microsoft\Command Processor" ^
+  /v Autorun ^
+  /t REG_EXPAND_SZ ^
+  /d %%UserProfile%%\.autorun.cmd ^
+  /f
+exit 0
