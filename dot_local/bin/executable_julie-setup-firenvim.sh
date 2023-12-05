390a5947d7c23a132dfbe6f96b3f621b1b7c7bb1
Julie Koubova <julie@koubova.net>
2023-12-05 23:30:41 +0100

    firenvim

--- dot_local/bin/executable_julie-setup-firenvim.sh
+++ dot_local/bin/executable_julie-setup-firenvim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if command -v nvim >/dev/null; then
   echo Installing Firenvim >&2
-  exec nvim -i NONE -c "lua require('firenvim')" -c "call firenvim#install(0)" -c qafi
+  exec nvim -i NONE -c "lua require('firenvim')" -c "call firenvim#install(0)" -c qa
 else
   echo Neovim not found >&2
   exit 1
