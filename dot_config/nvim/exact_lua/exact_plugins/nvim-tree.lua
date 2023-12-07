45fe47e6fff217e6d377c1f4302a65da0785c725
Julie Koubova <julie@koubova.net>
2023-12-07 23:03:51 +0100

    nvimtree

--- dot_config/nvim/exact_lua/exact_plugins/nvim-tree.lua
+++ dot_config/nvim/exact_lua/exact_plugins/nvim-tree.lua
@@ -1,31 +1,24 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  enabled = not vim.g.started_by_firenvim and not vim.g.vscode,
+  cond = not vim.g.started_by_firenvim and not vim.g.vscode,
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require('nvim-tree').setup({
-      actions = {
-        open_file = { quit_on_open = false },
+  hijack_directories = { enable = true },
+  lazy = false,
+  keys = {
+    { '<Leader>ft', '<Cmd>NvimTreeOpen<CR>', desc = 'NvimTree' },
+  },
+  opts = {
+    actions = {
+      open_file = { quit_on_open = false },
+    },
+    filters = {
+      custom = {
+        "^\\.git",
       },
-      filters = {
-        custom = {
-          "^\\.git",
-        },
-      },
-      git = { enable = false },
-      renderer = { group_empty = true, indent_markers = { enable = true } },
-      update_focused_file = { enable = true }
-    })
-
-    vim.api.nvim_create_autocmd(
-    { "VimEnter" },
-    { callback = function(data)
-      if vim.fn.isdirectory(data.file) == 1 then
-        vim.cmd.cd(data.file)
-        vim.cmd.enew()
-        require('nvim-tree.api').tree.open()
-      end
-    end })
-  end
+    },
+    git = { enable = false },
+    renderer = { group_empty = true, indent_markers = { enable = true } },
+    update_focused_file = { enable = true }
+  }
 }
 
