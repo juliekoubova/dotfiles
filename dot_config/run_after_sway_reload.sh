ca2c3c1b88c81a88d7288708beefe216458fdaee
Julie Koubova <julie@koubova.net>
2024-07-07 07:21:43 +0200

    sway exit

--- dot_config/run_after_sway_reload.sh
+++ dot_config/run_after_sway_reload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-command -v swaymsg >/dev/null || exit
-[ -n "${SWAYSOCK}" ] || exit
+command -v swaymsg >/dev/null || exit 0
+[ -n "${SWAYSOCK}" ] || exit 0
 
 echo "Reloading sway" >&2
 exec swaymsg reload
