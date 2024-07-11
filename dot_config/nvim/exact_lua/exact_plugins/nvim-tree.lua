0e65e9c7b1d9a74a696b75b8b57678edae6d71dd
Julie Koubova <julie@koubova.net>
2024-07-11 11:00:25 +0200

    vim

--- dot_config/nvim/exact_lua/exact_plugins/nvim-tree.lua
+++ dot_config/nvim/exact_lua/exact_plugins/nvim-tree.lua
@@ -2,7 +2,6 @@ return {
   "nvim-tree/nvim-tree.lua",
   cond = not (vim.g.started_by_firenvim or vim.g.vscode),
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  hijack_directories = { enable = true },
   lazy = false,
   keys = {
     { '<Leader>ft', '<Cmd>NvimTreeOpen<CR>', desc = 'NvimTree' },
@@ -20,7 +19,15 @@ return {
       },
     },
     git = { enable = false },
-    renderer = { group_empty = true, indent_markers = { enable = true } },
+    hijack_cursor = true,
+    hijack_directories = { enable = true },
+    renderer = {
+      group_empty = true,
+      indent_markers = { enable = true },
+    },
+    view = {
+      width = 30,
+    },
     update_focused_file = { enable = true }
   }
 }
--- dot_config/nvim/exact_lua/exact_plugins/toggleterm.lua
+++ dot_config/nvim/exact_lua/exact_plugins/toggleterm.lua
@@ -1,7 +1,10 @@
 return {
   "akinsho/toggleterm.nvim",
   opts = {
-    open_mapping = [[]],
+    open_mapping = { [[]], '<Leader>t' },
+    close_on_exit = true,
+    insert_mappings = false,
+    terminal_mappings = true,
     hide_numbers = true,
     auto_scroll = true,
     start_in_insert = true,
