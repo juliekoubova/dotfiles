4d3e51f2294addffb9a1fb854ad03149fb5503e5
Julie Koubova <julie@koubova.net>
2024-07-11 15:32:05 +0200

    lazydev

--- dot_config/nvim/exact_lua/exact_plugins/lspconfig.lua
+++ dot_config/nvim/exact_lua/exact_plugins/lspconfig.lua
@@ -5,31 +5,7 @@ return {
     local lsp = require('lspconfig')
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-    lsp.lua_ls.setup {
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using
-            -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {
-              'vim',
-              'require'
-            },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false,
-          },
-        }
-      }
-    }
-
+    lsp.lua_ls.setup { capabilities = capabilities }
     lsp.pylsp.setup { capabilities = capabilities }
     lsp.tsserver.setup { capabilities = capabilities }
     lsp.tailwindcss.setup { capabilities = capabilities }
