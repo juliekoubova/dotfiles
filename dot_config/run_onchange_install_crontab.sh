830da32c0a024c23afa6a5d97a4bb0910db52983
Julie Koubova <julie@koubova.net>
2024-07-12 20:43:02 +0200

    check for crontab

--- dot_config/run_onchange_install_crontab.sh
+++ dot_config/run_onchange_install_crontab.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # {{ include "dot_config.crontab" | sha256sum }}
 crontab="${XDG_CONFIG_HOME:-$HOME/.config}/crontab"
+command -v crontab >/dev/null || exit 0
 echo Installing crontab:
 cat "$crontab"
 exec crontab "$crontab"
