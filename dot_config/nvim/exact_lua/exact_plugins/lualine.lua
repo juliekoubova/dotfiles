1ede467ac66cd49a05c76c143ee3520a573354de
Julie Koubova <julie@koubova.net>
2025-11-09 17:32:16 +0100

    deprecated

--- dot_config/nvim/exact_lua/exact_plugins/lualine.lua
+++ dot_config/nvim/exact_lua/exact_plugins/lualine.lua
@@ -32,7 +32,7 @@ return {
     local lsp_server = {
       function()
         local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
+        local clients = vim.lsp.get_clients()
         if next(clients) == nil then
           return ''
         end
