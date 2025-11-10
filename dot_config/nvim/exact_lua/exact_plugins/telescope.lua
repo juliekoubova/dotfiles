8e4e1ecd80c7ac9f685667a69e4eef6034b405dc
Julie Koubova <julie@koubova.net>
2025-11-10 14:12:04 +0100

    fix fzf on windows

--- dot_config/nvim/exact_lua/exact_plugins/telescope.lua
+++ dot_config/nvim/exact_lua/exact_plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
 {
   'nvim-telescope/telescope-fzf-native.nvim',
   build =
-  'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+  'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
 },
 
   {
