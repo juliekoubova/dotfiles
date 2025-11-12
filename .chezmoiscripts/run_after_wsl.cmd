1ba6fd1b7ea37b25bd8b3f23b12fc8ff40c20139
Julie Koubova <julie@koubova.net>
2025-11-12 18:16:38 +0100

    win packages

--- .chezmoiscripts/run_after_wsl.cmd
+++ .chezmoiscripts/run_after_wsl.cmd
@@ -2,7 +2,7 @@
 Echo Windows Subsystem for Linux
 wsl --status >nul 2>&1
 If %ErrorLevel% Equ 0 (
-  sudo --disable-input wsl --update
+  wsl --update
 ) Else (
-  sudo --disable-input wsl --install --no-distribution
+  wsl --install --no-distribution
 )
