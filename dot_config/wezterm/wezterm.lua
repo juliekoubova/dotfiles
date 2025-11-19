1a1ca2afb08d092c5108bafb3dfaaa2a37b8d9ac
Julie Koubova <julie@koubova.net>
2025-11-19 16:34:40 +0100

    mac

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,15 +1,16 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.color_scheme = "Catppuccin Mocha"
+config.cursor_blink_rate = 0
 config.font = wezterm.font("Cascadia Code NF", { weight = "DemiLight" })
-config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.native_macos_fullscreen_mode = true
+config.prefer_to_spawn_tabs = true
 config.tab_bar_at_bottom = true
 config.tab_max_width = 42
-config.color_scheme = "Catppuccin Mocha"
+config.use_fancy_tab_bar = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.cursor_blink_rate = 0
-config.prefer_to_spawn_tabs = true
 
 if wezterm.target_triple:match("-windows-") then
   config.default_cwd = "c:\\src"
